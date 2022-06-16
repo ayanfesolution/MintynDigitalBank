bb45f671fe616bffb7e213a0483811086762976c
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-06-16 16:49:23 +0100

    set the initialView to the WelcomeViewController

--- MintynDigitalBank/MintynDigitalBank/Delegates/SceneDelegate.swift
+++ MintynDigitalBank/MintynDigitalBank/Delegates/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = scene as? UIWindowScene else {
             return
         }
-        let initialView = ViewController()
+        let initialView = WelcomeScreenViewController()
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = initialView
