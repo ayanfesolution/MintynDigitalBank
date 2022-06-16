b462e63a711f401f89e0a07543dc5eef6bb1503b
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-06-16 17:10:44 +0100

    create the logo image view

--- MintynDigitalBank/MintynDigitalBank/ViewControllers/WelcomeScreenViewController.swift
+++ MintynDigitalBank/MintynDigitalBank/ViewControllers/WelcomeScreenViewController.swift
@@ -8,11 +8,41 @@
 import UIKit
 
 class WelcomeScreenViewController: UIViewController {
-
+    // MARK: - Properties (Created Using IIFE: Immediately Invoked Function Expression)
+    
+    // LogoImage
+    let logoImageView: UIImageView = {
+        let logo = UIImageView()
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        logo.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        logo.image = UIImage(named: "welcomelogo")
+        return logo
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .black
+        setUPView()
         // Do any additional setup after loading the view.
+    }
+    
+    func setUPView() {
+        let uiViews = [logoImageView]
+        for uiView in uiViews {
+            view.addSubview(uiView)
+        }
+        //MARK: - Setting Up Constraints for the UIViews
+        NSLayoutConstraint.activate([
+            // Constraints for logoImageView
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100)
+        ])
+        
+        
+        
+        
     }
 }
 
