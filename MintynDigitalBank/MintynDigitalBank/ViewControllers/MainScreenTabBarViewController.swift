304e3bfb5dd0a7b35c137905a36f4e8d0eb5d4b4
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-06-17 04:46:54 +0100

    create the needed setup for tab bar controller

--- MintynDigitalBank/MintynDigitalBank/ViewControllers/MainScreenTabBarViewController.swift
+++ MintynDigitalBank/MintynDigitalBank/ViewControllers/MainScreenTabBarViewController.swift
@@ -11,7 +11,28 @@ class MainScreenTabBarViewController: UITabBarController {
     // MARK: - Properties
     let home = HomePageViewController()
     let settings = SettingsPageViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTabBarViews()
     }
+    
+    func setupTabBarViews() {
+        tabBar.tintColor = UIColor.CustomColor.tabBarIconColorActive
+        tabBar.unselectedItemTintColor = UIColor.CustomColor.tabBarIconColorNotActive
+        tabBar.layer.shadowColor = UIColor.lightGray.cgColor
+        tabBar.layer.shadowOpacity = 0.2
+        tabBar.layer.shadowRadius = 5
+        tabBar.clipsToBounds = false
+        tabBar.backgroundColor = UIColor.white
+        home.title = "Home"
+        settings.title = "Settings"
+        setViewControllers([home, settings], animated: false)
+        guard let items = tabBar.items else { return }
+        let tabBarIcons = ["house.fill", "gearshape.fill"]
+        for icon in 0 ..< items.count {
+            items[icon].image = UIImage(systemName: tabBarIcons[icon])
+        }
+    }
+
 }
