4896f2c9bb35ea7eddbc64a226331273256703a1
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-06-18 04:02:54 +0100

    perfect the code

--- MintynDigitalBank/MintynDigitalBank/ViewControllers/SystemViewController.swift
+++ MintynDigitalBank/MintynDigitalBank/ViewControllers/SystemViewController.swift
@@ -93,7 +93,7 @@ extension SystemViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let cell = tableView.dequeueReusableCell(withIdentifier: reusableCell, for: indexPath)
+        
         switch indexPath.row {
         case 0:
             UIApplication.shared.windows.forEach { windowSence in
