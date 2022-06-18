ba3ddf36125a5188f4f8715401120ceed7a0632b
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-06-18 03:34:39 +0100

    create the system page

--- MintynDigitalBank/MintynDigitalBank/ViewControllers/SystemViewController.swift
+++ MintynDigitalBank/MintynDigitalBank/ViewControllers/SystemViewController.swift
@@ -8,22 +8,90 @@
 import UIKit
 
 class SystemViewController: UIViewController {
-
+    // MARK: - Properties (Created Using IIFE: Immediately Invoked Function Expression)
+    let reusableCell = "systemPageCell"
+    let legalData = ["Default", "Light Mode", "Dark Mode"]
+    //  Navigation Button
+    lazy var navigationBtn: UIButton = {
+        let navBtn = UIButton()
+        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: UIImage.SymbolWeight.bold)
+        navBtn.translatesAutoresizingMaskIntoConstraints = false
+        navBtn.setImage(UIImage(systemName: "chevron.backward", withConfiguration: config), for: .normal)
+        navBtn.addTarget(self, action: #selector(dismissTheView), for: .touchUpInside)
+        return navBtn
+    }()
+    // Header Title
+    lazy var navTitleHeader: UILabel = {
+        let navTitle = UILabel()
+        navTitle.translatesAutoresizingMaskIntoConstraints = false
+        navTitle.text = "System"
+        navTitle.textColor = UIColor.CustomColor.textColorGray
+        navTitle.font = customFont(font: .robotoBlack, size: 24)
+        return navTitle
+    }()
+    lazy var systemListTableView: UITableView = {
+        let systemListTableView = UITableView()
+        systemListTableView.translatesAutoresizingMaskIntoConstraints = false
+        systemListTableView.delegate = self
+        systemListTableView.dataSource = self
+        systemListTableView.separatorStyle = .none
+        systemListTableView.register(UITableViewCell.self, forCellReuseIdentifier: reusableCell)
+        return systemListTableView
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setUPView()
+    }
+    @objc func dismissTheView() {
+        dismiss(animated: true)
+    }
+    func setUPView() {
+        let uiViews = [navTitleHeader, navigationBtn, systemListTableView]
+        for uiView in uiViews {
+            view.addSubview(uiView)
+        }
+        //MARK: - Setting Up Constraints for the UIViews
+        NSLayoutConstraint.activate([
+            //  constraints for Navigation Button
+            navigationBtn.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            navigationBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            // constraints for Header
+            navTitleHeader.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            navTitleHeader.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            // constraints for settingListTableView
+            systemListTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            systemListTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            systemListTableView.topAnchor.constraint(equalTo: navTitleHeader.bottomAnchor, constant: 15),
+            systemListTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            ])
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension SystemViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return legalData.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .bold)
+        let arrowImage = UIImageView(image: UIImage(systemName: "square.and.arrow.up.trianglebadge.exclamationmark", withConfiguration: config))
+        arrowImage.tintColor = UIColor.CustomColor.textColorGray
+        let cell = tableView.dequeueReusableCell(withIdentifier: reusableCell, for: indexPath)
+        let lists = legalData[indexPath.row]
+        cell.textLabel?.text = lists
+        cell.textLabel?.textColor = UIColor.CustomColor.textColorGray
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        return cell
     }
-    */
-
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Appearance"
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      
+    }
 }
